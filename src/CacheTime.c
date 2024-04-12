a78a33af23aff666a5bb6fba54448ca64efc8a30
Zenith Zack <faldund@outlook.com>
2024-04-11 23:58:32 -0400

    feat(meltdown-attack): replace cachetime function

--- src/CacheTime.c
+++ src/CacheTime.c
@@ -2,54 +2,31 @@
 
 #include <emmintrin.h>
 #include <x86intrin.h>
-uint8_t array[256 * 4096];
-int temp;
-char secret = 94;
-/* cache hit time threshold assumed*/
-#define CACHE_HIT_THRESHOLD (80)
-#define DELTA 1024
 
-void flushSideChannel()
-{
-    int i;
-    // Write to array to bring it to RAM to prevent Copy-on-write
-    for (i = 0; i < 256; i++)
-        array[i * 4096 + DELTA] = 1;
+uint8_t array[10 * 4096];
 
-    // Flush the values of the array from cache
-    for (i = 0; i < 256; i++)
-        _mm_clflush(&array[i * 4096 + DELTA]);
-}
-
-void victim()
-{
-    temp = array[secret * 4096 + DELTA];
-}
-
-void reloadSideChannel()
+int main(int argc, const char **argv)
 {
     int junk = 0;
     register uint64_t time1, time2;
     volatile uint8_t *addr;
     int i;
-    for (i = 0; i < 256; i++)
+    // Initialize the array
+    for (i = 0; i < 10; i++)
+        array[i * 4096] = 1;
+    // FLUSH the array from the CPU cache
+    for (i = 0; i < 10; i++)
+        _mm_clflush(&array[i * 4096]);
+    // Access some of the array items
+    array[3 * 4096] = 100;
+    array[7 * 4096] = 200;
+    for (i = 0; i < 10; i++)
     {
-        addr = &array[i * 4096 + DELTA];
+        addr = &array[i * 4096];
         time1 = __rdtscp(&junk);
-        junk = *addr;
+        ¿ junk = *addr;
         time2 = __rdtscp(&junk) - time1;
-        if (time2 <= CACHE_HIT_THRESHOLD)
-        {
-            printf("array[%d*4096 + %d] is in cache.\n", i, DELTA);
-            printf("The Secret = %d.\n", i);
-        }
+        ¡ printf("Access time for array[%d*4096]: %d CPU cycles\n", i, (int)time2);
     }
-}
-
-int main(int argc, const char **argv)
-{
-    flushSideChannel();
-    victim();
-    reloadSideChannel();
-    return (0);
+    return 0;
 }
